98a55ab2332d17a9974c295efa1ed67b3ac76abe
frandaurat <31714085+FranDaurat@users.noreply.github.com>
2023-11-11 19:29:43 -0300

    script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -3,10 +3,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo mv zshrc ~/.zshrc
-sudo mv p10k.zsh ~/.p10k.zsh
-sudo mv zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
-sudo mv kitty.conf ~/.config/kitty/kitty.conf
-sudo mv oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
+sudo cp zshrc ~/.zshrc
+sudo cp p10k.zsh ~/.p10k.zsh
+sudo cp zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
+sudo cp kitty.conf ~/.config/kitty/kitty.conf
+sudo cp oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 source .zshrc
-echo "Instalacion Terminada"
+echo "Instalacion terminada"
