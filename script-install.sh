c0d50d3c31957b3346e7dc33b54f1ddef5e717df
frandaurat <31714085+FranDaurat@users.noreply.github.com>
2023-11-11 14:32:52 -0300

    Add files via upload

---
+++ script-install.sh
@@ -0,0 +1,11 @@
+sudo apt update -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo mv zshrc ~/.zshrc
+sudo mv p10k.zsh ~/.p10k.zsh
+sudo mv zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
+sudo mv kitty.conf ~/.config/kitty/kitty.conf
+source .zshrc
+echo "Instalacion Terminada"
