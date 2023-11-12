59ebec46f40495f836c31fa8a68b820dcc67b6f8
frandaurat <31714085+FranDaurat@users.noreply.github.com>
2023-11-12 11:45:51 -0300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -3,6 +3,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo rm ~/.zshrc
+sudo rm ~/.p10k.zsh
+sudo rm  ~/.config/kitty/kitty.conf
 sudo cp zshrc ~/.zshrc
 sudo cp p10k.zsh ~/.p10k.zsh
 sudo cp kitty.conf ~/.config/kitty/kitty.conf
