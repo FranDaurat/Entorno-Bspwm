b752e5628382c3bcd52270c65c5a62776788aeef
Your Name <you@example.com>
2024-09-06 20:16:17 -0300

    Pusheo Automatizado

---
+++ pushing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+rm -rf ./gobin/nuclei 
+
+git add . 
+git commit -m "Pusheo Automatizado"
+git push origin main
+
+
--- update.sh
+++ update.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sudo cp -r /opt/tools/* /home/elgordoponcio/entorno-bspwm/tools/
-sudo cp -r /home/elgordoponcio/go/bin/* /home/elgordoponcio/entorno-bspwm/gobin/
+# Copia de archivos
+sudo cp -r /home/elgordoponcio/go/bin/* /home/elgordoponcio/repos/entorno-bspwm/gobin/
 sudo cp /home/elgordoponcio/.zshrc zshrc
+
+# Git Push
+rm -rf ./gobin/nuclei 
+
+git add . 
+git commit -m "Pusheo Automatizado"
+git push origin main
+
+
 
